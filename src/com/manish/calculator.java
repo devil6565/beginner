c180c5f1325144ac738a8245310663b203f8fe76
devil6565 <mk491936@gmail.com>
2021-09-22 23:45:31 +0530

    basic calculator.java

---
+++ src/com/manish/calculator.java
@@ -0,0 +1,16 @@
+package com.manish;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void amin(String[] args){
+        System.out.println("entre the num1 opretor num2:");
+        Scanner n=new Scanner(System.in);
+        double d1=n.nextDouble();
+        double d2=n.nextDouble();
+        System.out.println(d1 + d2);
+
+
+
+    }
+}
